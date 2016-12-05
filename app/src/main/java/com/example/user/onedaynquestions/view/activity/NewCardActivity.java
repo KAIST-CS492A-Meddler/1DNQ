e629f43d2a8ba14a7e479f2be5efe96a7d58a119
BaekHorang <ymbaek@se.kaist.ac.kr>
2016-12-06 00:08:43 +0900

    NewCardActivity 이벤트 처리 및 위젯 인스턴스화

--- app/src/main/java/com/example/user/onedaynquestions/view/activity/NewCardActivity.java
+++ app/src/main/java/com/example/user/onedaynquestions/view/activity/NewCardActivity.java
@@ -6,6 +6,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.example.user.onedaynquestions.R;
@@ -21,15 +25,19 @@ public class NewCardActivity extends AppCompatActivity {
     MyRoutine selectedRoutine;
 
     public static Activity thisActivity;
-//    ImageView startexercise_iv_girl;
-//    ImageView startexercise_iv_start;
-//
-//    TextView tv_routine_name;
-//    TextView tv_routine_eq1;
-//    TextView tv_routine_eq2;
-//    TextView tv_routine_eq3;
-//    TextView tv_routine_eq4;
-//    TextView tv_routine_eq5;
+
+    EditText newCard_et_maker;
+    EditText newCard_et_datetime;
+    EditText newCard_et_question;
+    EditText newCard_et_answer;
+    EditText newCard_et_hint;
+
+    Spinner newCard_spinner_type;
+
+    Button newCard_btn_add;
+
+    private ArrayAdapter<String> mSpinnerAdapter = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,159 +57,34 @@ public class NewCardActivity extends AppCompatActivity {
 
 
 
-        Log.d("parseRoutineGoal", parseRoutineGoal(3, "3|10|-1"));
-        Log.d("parseRoutineGoal", parseRoutineGoal(3, "5|-1|120"));
-        Log.d("parseRoutineGoal", parseRoutineGoal(3, "1|30|-1"));
-        Log.d("parseRoutineGoal", parseRoutineGoal(3, "1|-1|30"));
-
-
-
         thisActivity = this;
 
-//        startexercise_iv_girl = (ImageView) findViewById(R.id.startexercise_iv_girl);
-//        startexercise_iv_start = (ImageView) findViewById(R.id.startexercise_iv_start);
-//
-//        //For animation + activity transition
-//        startexercise_iv_start.setOnTouchListener(new View.OnTouchListener() {
-//            @Override
-//            public boolean onTouch(View v, MotionEvent event) {
-//                startexercise_iv_girl.setImageResource(R.drawable.start_exercise_girl_anim);
-//
-//                Handler mHandler = new Handler();
-//                mHandler.postDelayed(new Runnable() {
-//                    @Override
-//                    public void run() {
-//                        startexercise_iv_girl.setImageResource(R.drawable.start_exercise_girl);
-//                        try {
-//                            Thread.sleep(100);
-//
-////                            Intent intent_doingexercise = new Intent(getApplicationContext(), DoingExerciseActivity.class);
-////                            startActivity(intent_doingexercise);
-////                            NewCardActivity.this.finish();
-//                        } catch (InterruptedException e) {
-//                            e.printStackTrace();
-//                        }
-//                    }
-//                }, 50);
-//
-//                return false;
-//            }
-//        });
 
     }
 
 
     private void initWidgets() {
 
-//        tv_routine_name = (TextView) findViewById(R.id.startexercise_tv_myroutine);
-//        tv_routine_eq1 = (TextView) findViewById(R.id.startexercise_tv_eq1);
-//        tv_routine_eq2 = (TextView) findViewById(R.id.startexercise_tv_eq2);
-//        tv_routine_eq3 = (TextView) findViewById(R.id.startexercise_tv_eq3);
-//        tv_routine_eq4 = (TextView) findViewById(R.id.startexercise_tv_eq4);
-//        tv_routine_eq5 = (TextView) findViewById(R.id.startexercise_tv_eq5);
+        newCard_et_maker = (EditText) findViewById(R.id.newcard_et_maker);
+        newCard_et_datetime = (EditText) findViewById(R.id.newcard_et_datetime);
+        newCard_et_question = (EditText) findViewById(R.id.newcard_et_question);
+        newCard_et_answer = (EditText) findViewById(R.id.newcard_et_answer);
+        newCard_et_hint = (EditText) findViewById(R.id.newcard_et_hint);
+
+        newCard_spinner_type = (Spinner) findViewById(R.id.newcard_spinner_type);
+
+        newCard_btn_add = (Button) findViewById(R.id.newcard_btn_add);
+
     }
 
     private void initWidetValues() {
 
-//        //Initialize equipment information
-//        if (selectedRoutine != null) {
-//            tv_routine_name.setText(selectedRoutine.getRoutineName());
-//
-//            //Initialize equipment names
-//            String routine_eq1_id = selectedRoutine.getRoutineEq1Id();
-//            String routine_eq2_id = selectedRoutine.getRoutineEq2Id();
-//            String routine_eq3_id = selectedRoutine.getRoutineEq3Id();
-//            String routine_eq4_id = selectedRoutine.getRoutineEq4Id();
-//            String routine_eq5_id = selectedRoutine.getRoutineEq5Id();
-//
-//            String eq1String = "";
-//            String eq2String = "";
-//            String eq3String = "";
-//            String eq4String = "";
-//            String eq5String = "";
-//
-//            if (!routine_eq1_id.equals("-1")) {
-//                String routine_eq1_name = MainActivity.hereDB.getAgentNameByMacId(routine_eq1_id);
-//                if (routine_eq1_name != null) {
-//                    eq1String = routine_eq1_name;
-//                } else {
-//                    eq1String = "DB failed";
-//                }
-//            }
-//
-//            if (!routine_eq2_id.equals("-1")) {
-//                String routine_eq2_name = MainActivity.hereDB.getAgentNameByMacId(routine_eq2_id);
-//                if (routine_eq2_name != null) {
-//                    eq2String = routine_eq2_name;
-//                }else {
-//                    eq2String = "DB failed";
-//                }
-//            }
-//
-//            if (!routine_eq3_id.equals("-1")) {
-//                String routine_eq3_name = MainActivity.hereDB.getAgentNameByMacId(routine_eq3_id);
-//                if (routine_eq3_name != null) {
-//                    eq3String = routine_eq3_name;
-//                }else {
-//                    eq3String = "DB failed";
-//                }
-//            }
-//
-//            if (!routine_eq4_id.equals("-1")) {
-//                String routine_eq4_name = MainActivity.hereDB.getAgentNameByMacId(routine_eq4_id);
-//                if (routine_eq4_name != null) {
-//                    eq4String = routine_eq4_name;
-//                }else {
-//                    eq4String = "DB failed";
-//                }
-//            }
-//
-//            if (!routine_eq5_id.equals("-1")) {
-//                String routine_eq5_name = MainActivity.hereDB.getAgentNameByMacId(routine_eq5_id);
-//                if (routine_eq5_name != null) {
-//                    eq5String = routine_eq5_name;
-//                }else {
-//                    eq5String = "DB failed";
-//                }
-//            }
-//
-//
-//            //Initialize goal of equipments & setText
-//            if (!eq1String.equals("")) {
-//                eq1String += ": " + parseRoutineGoal(3, selectedRoutine.getRoutineEq1Goal());
-//                tv_routine_eq1.setText(eq1String);
-//            } else {
-//                tv_routine_eq1.setVisibility(View.INVISIBLE);
-//            }
-//            if (!eq2String.equals("")) {
-//                eq2String += ": " + parseRoutineGoal(3, selectedRoutine.getRoutineEq2Goal());
-//                tv_routine_eq2.setText(eq2String);
-//            } else {
-//                tv_routine_eq2.setVisibility(View.INVISIBLE);
-//            }
-//            if (!eq3String.equals("")) {
-//                eq3String += ": " + parseRoutineGoal(3, selectedRoutine.getRoutineEq3Goal());
-//                tv_routine_eq3.setText(eq3String);
-//            } else {
-//                tv_routine_eq3.setVisibility(View.INVISIBLE);
-//            }
-//            if (!eq4String.equals("")) {
-//                eq4String += ": " + parseRoutineGoal(3, selectedRoutine.getRoutineEq4Goal());
-//                tv_routine_eq4.setText(eq4String);
-//            } else {
-//                tv_routine_eq4.setVisibility(View.INVISIBLE);
-//            }
-//            if (!eq5String.equals("")) {
-//                eq5String += ": " + parseRoutineGoal(3, selectedRoutine.getRoutineEq5Goal());
-//                tv_routine_eq5.setText(eq5String);
-//            }else {
-//                tv_routine_eq5.setVisibility(View.INVISIBLE);
-//            }
-//
-//
-//        } else {
-//            tv_routine_name.setText("There is no received routine.");
-//        }
+        mSpinnerAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,
+                (String[])getResources().getStringArray(R.array.spinner_list));
+        mSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        newCard_spinner_type.setAdapter(mSpinnerAdapter);
+
+        newCard_spinner_type.setSelection(0);
     }
 
 
@@ -284,11 +167,17 @@ public class NewCardActivity extends AppCompatActivity {
     }
 
     public void mOnClick(View v) {
-//        switch (v.getId()) {
-//            case R.id.startexercise_btn_back:
-//                finish();
-//                break;
-//        }
+        switch (v.getId()) {
+            case R.id.newcard_btn_add:
+
+                int cardType;
+
+                cardType = newCard_spinner_type.getSelectedItemPosition();
+
+
+                Toast.makeText(getApplicationContext(), "Make a new card\nInsert card data to Server DB\nSelect saved card data\nInsert selected card data to local DB", Toast.LENGTH_LONG).show();
+                break;
+        }
     }
 
 }
