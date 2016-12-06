53d1e9a410382422415be3a9d864f04c7d0e2bb1
orchardthief <phyntoswasp@gmail.com>
2016-12-06 23:18:47 +0900

    Add files via upload

---
+++ DB/create_sample.php
@@ -0,0 +1,11 @@
+﻿<?php 
+    include_once("connection.php");
+	if($_POST['sample_id']){
+	$temp = $_POST['sample_id'];
+	$query = "INSERT INTO sample (sample_id) VALUES ('$temp')";
+
+	}else{
+	$query = "INSERT INTO sample (sample_id) VALUES ('NO_ISSET')";
+	}
+	mysqli_query($conn, $query);
+?>
---
+++ DB/get_user_app.php
@@ -0,0 +1,4 @@
+﻿<form action="get_user.php" method="post">
+ <p>User id: <input type="text" name="userinfo_id" /></p>
+ <p><input type="submit" /></p>
+</form>
